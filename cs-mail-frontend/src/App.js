314043d557335555aa9a08175ba14f0557e5d336
nngocjade <nngocjade@gmail.com>
2021-02-27 13:40:42 +0700

    added react app setup

---
+++ cs-mail-frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="text-center">
+      <Container>
+        <h1>My new React App</h1>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
