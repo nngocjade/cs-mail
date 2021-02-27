56b69338a73436295586a5e156915379b8380d98
nngocjade <nngocjade@gmail.com>
2021-02-27 11:33:33 +0700

    added token for user authentication

--- models/User.js
+++ models/User.js
@@ -1,6 +1,25 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const jwt = require("jsonwebtoken");
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+
+userSchema.methods.toJSON = function () {
+  const obj = this._doc;
+  delete obj.password;
+  delete obj.emailVerified;
+  delete obj.emailVerificationCode;
+  delete obj.isDeleted;
+  return obj;
+};
+
+userSchema.methods.generateToken = async function () {
+  const accessToken = await jwt.sign({ _id: this._id }, JWT_SECRET_KEY, {
+    expiresIn: "1d",
+  });
+  return accessToken;
+};
+
 const userSchema = Schema(
   {
     name: { type: String, required: true },
